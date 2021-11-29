7d89dfd9b069bb190097b47f9177def04d96f351
AntonioMontanhaEstadao <antonio.filho.mckinsey@estadao.com>
2021-11-29 15:28:39 -0300

    Starting terraforms studies

---
+++ main.tf
@@ -0,0 +1,130 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.66.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "MontanhaVpc"
+    Demo = "Terraform"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet1"
+    Type = "Public" // what is the purpose of this tag?
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "subnet1"
+    Type = "Public"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    "Name"  = "Main"
+    "Owner" = "MontanhaVpc"
+  }
+}
+
+resource "aws_route_table" "rt1" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "Public"
+  }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rt1.id
+}
+
+resource "aws_route_table_association" "rta2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.rt1.id
+}
+
+resource "aws_security_group" "webserver" {
+  name        = "webserver"
+  description = "webserver network trafic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.workstation_ip] // O que é isso?
+  }
+
+  ingress {
+    description = "80 from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Allow traffic" // Essa tag faz algo?
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = var.amis[var.region]
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = aws_subnet.subnet1.id
+  vpc_security_group_ids = [aws_security_group.webserver.id]
+
+  associate_public_ip_address = true
+
+  #userdata
+  user_data = <<EOF
+#!/bin/bash
+apt-get -y update
+apt-get -y install nginx
+service nginx start
+echo fin v1.00!
+EOF
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
